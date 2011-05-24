60cbc743cf253d6a73660d98a798ce4fb0c709f5
Adam Derewecki <derewecki@gmail.com>
2011-05-23 21:55:41 -0700

    initial

---
+++ remote_torrentify.js
@@ -0,0 +1,14 @@
+var torrent_prefix = "http://www.demonoid.me/files/details/"; //2616152/1073442/
+torrent_prefix = "/files/details/";
+var anchors = document.getElementsByTagName('a');
+for (var idx in anchors) {
+  var anchor = anchors[idx];
+  if (anchor.href === undefined) { continue; }
+  if (anchor.href.substring(0, torrent_prefix.length) == torrent_prefix) {
+    console.log(anchor.href);
+    anchor.href = "http://www.google.com";
+    anchor.style = "display: none;";
+
+  }
+}
+
