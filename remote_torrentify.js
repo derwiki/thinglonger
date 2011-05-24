e9b97bc9e83127ad9f66898d1cef015277414470
Adam Derewecki <derewecki@gmail.com>
2011-05-24 09:59:33 -0700

    Better string startswith

--- remote_torrentify.js
+++ remote_torrentify.js
@@ -1,11 +1,11 @@
 console.log('Executing remote_torrentify.js...');
-var torrent_prefix = "http://www.demonoid.me/files/details/"; //2616152/1073442/
-torrent_prefix = "/files/details/";
+//var torrent_prefix = "http://www.demonoid.me/files/details/"; //2616152/1073442/
+var torrent_prefix = "/files/details/";
 var anchors = document.getElementsByTagName('a');
 for (var idx in anchors) {
   var anchor = anchors[idx];
   if (anchor.href === undefined) { continue; }
-  if (anchor.href.substring(0, torrent_prefix.length) == torrent_prefix) {
+  if (anchor.href.indexOf(torrent_prefix) === 0) {
     console.log(anchor.href);
     anchor.href = "http://www.google.com";
     anchor.style = "display: none;";
