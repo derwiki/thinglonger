e8a3e9d1812b2380150c08f12b197072d73da3f6
Adam Derewecki <derewecki@gmail.com>
2011-05-24 20:41:13 -0700

    Fixed up JS

--- remote_torrentify.js
+++ remote_torrentify.js
@@ -1,15 +1,11 @@
+// /file/details/2616152/1073442/
 console.log('Executing remote_torrentify.js...');
-//var torrent_prefix = "http://www.demonoid.me/files/details/"; //2616152/1073442/
 var torrent_prefix = "/files/details/";
 var anchors = document.getElementsByTagName('a');
-for (var idx in anchors) {
+for (var idx = 0; idx < anchors.length; idx++) {
   var anchor = anchors[idx];
-  if (anchor.href === undefined) { continue; }
-  if (anchor.href.indexOf(torrent_prefix) === 0) {
-    console.log(anchor.href);
+  if (anchor.href.indexOf(torrent_prefix) !== -1) {
     anchor.href = "http://www.google.com";
     anchor.style = "display: none;";
-
   }
 }
-
