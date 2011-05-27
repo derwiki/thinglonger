85ea8af47e6dec664ec51b364e7272ec72f670eb
Adam Derewecki <derewecki@gmail.com>
2011-05-27 18:01:18 -0400

    Made server.py a little better and .gitignored server.log

--- server.py
+++ server.py
@@ -9,6 +9,7 @@ import time
 import bottle
 
 log = logging.getLogger('rtorrent_servlet')
+logging.basicConfig()
 log.setLevel(logging.DEBUG);
 
 bottle.debug(True)
@@ -16,8 +17,9 @@ bottle.debug(True)
 @bottle.route('/rtorrent', method="POST")
 def rtorrent():
     torrent_data = bottle.request.POST.get('torrent_data')
-    torrent_filename = bottle.request.POST.get('torrent_filename').replace(' ', '_')
-    with open('%s.torrent' % torrent_filename, 'w') as torrent:
+    torrent_filename = bottle.request.POST.get('torrent_filename').replace(' ', '_') + '.torrent'
+    log.info(torrent_filename)
+    with open(torrent_filename, 'w') as torrent:
         torrent.write(base64.b64decode(torrent_data))
     return dict(success=True)
 
